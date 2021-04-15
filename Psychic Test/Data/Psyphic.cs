70d1ee876eeb4b6c4e3b69c8f667ba3ce528afcd
Николай Михеев <miheevkolya@gmail.com>
2021-04-15 12:44:44 +0700

    Add project files.

---
+++ Psychic Test/Data/Psyphic.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Psychic_Test.Data
+{
+    public class Psyphic
+    {
+        static int Number;
+        public string Name { get; init; } = $"Экстрасенс {++Number}";
+        public float Credibility { get; protected set; }
+
+        private Random random;
+        private List<sbyte> _Guesses { get; init; } = new List<sbyte>();
+        public IReadOnlyList<sbyte> Guesses => _Guesses.AsReadOnly();
+
+        public Psyphic()
+        {
+            random = new Random(Number);
+        }
+
+        public sbyte GetNewGuess()
+        {
+            sbyte newguess = (sbyte)random.Next(10, 99);
+            _Guesses.Add(newguess);
+            return newguess;
+        }
+        public void AddCreditibility()
+        {
+            Credibility += 0.01f;
+        }
+        public void ReduceCreditibility()
+        {
+            Credibility -= 0.01f;
+        }
+    }
+}
